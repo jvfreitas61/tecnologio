2de42eff7b15b86a7afc003c627349686c76bc46
JV Freitas <134442764+jvfreitas61@users.noreply.github.com>
2025-05-28 01:36:39 -0300

    ajuste-mobile

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+function clickMenu() {
+    if (itens.style.display == 'flex') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'flex'
+    }
+}
+
+function fecharMenu(){
+  if (itens.style.display == 'flex'){
+    itens.style.display = 'none'
+  }
+}
+
+window.onresize = function() {
+        location.reload();
+};
